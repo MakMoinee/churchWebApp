65e120870d8659e72788546d804315f60a3c2def
DESKTOP-LKF81KV\Mak Moinee <titus.silver.ph@gmail.com>
2022-11-29 15:23:23 +0800

    update code

--- churchdb.sql
+++ churchdb.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : Local
+ Source Server         : LOCAL
  Source Server Type    : MySQL
  Source Server Version : 80030 (8.0.30)
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 80030 (8.0.30)
  File Encoding         : 65001
 
- Date: 13/10/2022 14:09:57
+ Date: 29/11/2022 15:23:15
 */
 
 SET NAMES utf8mb4;
@@ -29,12 +29,11 @@ CREATE TABLE `church_users`  (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`userID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of church_users
 -- ----------------------------
-INSERT INTO `church_users` VALUES (1, 'admin', '$2y$14$gVwmYuaMGBPH9xsRx8y5iePZQ9RcrMtBkNXn5f4kHuCRjYWyGfB4O', '1', '2022-10-09 23:53:44', '2022-10-09 23:53:48');
 
 -- ----------------------------
 -- Table structure for failed_jobs
@@ -65,7 +64,7 @@ CREATE TABLE `migrations`  (
   `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of migrations
@@ -75,7 +74,8 @@ INSERT INTO `migrations` VALUES (2, '2014_10_12_100000_create_password_resets_ta
 INSERT INTO `migrations` VALUES (3, '2019_08_19_000000_create_failed_jobs_table', 1);
 INSERT INTO `migrations` VALUES (4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
 INSERT INTO `migrations` VALUES (5, '2022_10_09_145915_create_church_users_table', 1);
-INSERT INTO `migrations` VALUES (6, '2022_10_09_233456_create_roles_table', 2);
+INSERT INTO `migrations` VALUES (6, '2022_10_09_233456_create_roles_table', 1);
+INSERT INTO `migrations` VALUES (7, '2022_11_12_170019_create_transactions_table', 1);
 
 -- ----------------------------
 -- Table structure for password_resets
@@ -122,18 +122,36 @@ DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles`  (
   `userType` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `members` tinyint(1) NOT NULL,
+  `users` tinyint(1) NOT NULL,
   `inputs` tinyint(1) NOT NULL,
-  `announcements` tinyint(1) NOT NULL,
+  `chats` tinyint(1) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`userType`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of roles
 -- ----------------------------
-INSERT INTO `roles` VALUES (1, 'administrator', 1, 1, 1, '2022-10-10 07:43:22', '2022-10-10 07:43:25');
+
+-- ----------------------------
+-- Table structure for transactions
+-- ----------------------------
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE `transactions`  (
+  `transactionID` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` double(8, 2) NOT NULL,
+  `transactionDate` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`transactionID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of transactions
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for users
@@ -160,6 +178,6 @@ CREATE TABLE `users`  (
 -- View structure for vwuserswithroles
 -- ----------------------------
 DROP VIEW IF EXISTS `vwuserswithroles`;
-CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `vwuserswithroles` AS select `church_users`.`userID` AS `userID`,`church_users`.`username` AS `username`,`roles`.`userType` AS `userType`,`roles`.`description` AS `description`,`roles`.`members` AS `members`,`roles`.`inputs` AS `inputs`,`roles`.`announcements` AS `announcements` from (`church_users` join `roles` on((`church_users`.`userType` = `roles`.`userType`)));
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `vwuserswithroles` AS select `church_users`.`userID` AS `userID`,`church_users`.`username` AS `username`,`roles`.`userType` AS `userType`,`roles`.`description` AS `description`,`roles`.`users` AS `users`,`roles`.`inputs` AS `inputs`,`roles`.`chats` AS `chats` from (`church_users` join `roles` on((`church_users`.`userType` = `roles`.`userType`)));
 
 SET FOREIGN_KEY_CHECKS = 1;
